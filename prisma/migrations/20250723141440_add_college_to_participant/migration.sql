a794c5ec5c65dadc937493898199f9af19ec4e24
Mahadevan Reji <rmahadevan574@gmail.com>
2025-07-23 23:17:46 +0530

    Remove obsolete migration files for initial setup and college column addition in participants table

--- prisma/migrations/20250723141440_add_college_to_participant/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `college` to the `participants` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "participants" ADD COLUMN     "college" TEXT NOT NULL;
